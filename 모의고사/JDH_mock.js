182fe263a54df6278b72b5994e1e769cc3491d6b
tichdaen <tichdaen@gmail.com>
2021-11-29 23:55:15 +0900

    후 진짜 에바네

--- 모의고사/JDH_mock.js
+++ 모의고사/JDH_mock.js
@@ -12,36 +12,35 @@ rl.on('line', line => {
 const solution = answer => {
     answer = answer.replace(/[\]\[(\s)]/g, '').split(',').map(v => Number(v));
     
-    let man1 = answer => {
-        let score = 0;
-        let method = 1;
-        answer.forEach(a => {
-            console.log(a, method);
-            if ( a == method++ ) score++;
-            if ( method > 5 ) method = 1;
-        });
+    let result = [0, 0, 0];
 
-        return score;
+    let getB = i => {
+        if ( i % 2 == 0 ) return 2;
+        else if ( i == 5 ) return 4;
+        else if ( i == 7 ) return 5;
+        else return i;
     }
 
-    let man2 = answer => {
-        let score = 0;
-        let method = 1;
-        answer.forEach((item, index) => {
-            if ( index / 2 == 0 ) {
-                score = item == 2 ? ++score : score;
-            } else {
-                score = item == method++ ? ++score : score;
-            }
-        });
-
-        return score;
+    let getC = i => {
+        if ( i < 2 ) return 3
+        else if ( i < 4 ) return 1
+        else if ( i < 6 ) return 2
+        else if ( i < 8 ) return 4
+        else if ( i < 10 ) return 5
     }
 
-    let man3 = answer => {
-        
+    let i = 0;
+    for (; i < answer.length; i++) {
+        let a, b, c;
+
+        a = ( i % 5 ) + 1;
+        b = getB(i % 8);
+        c = getC(i % 10);
+
+        result[0] = a == answer[i] ? ++result[0] : result[0];
+        result[1] = b == answer[i] ? ++result[1] : result[1];
+        result[2] = c == answer[i] ? ++result[2] : result[2];
     }
 
-    console.log(man1(answer));
-    console.log(man2(answer));
+    
 }
