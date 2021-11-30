dc694ce1f5b324d59e1c53aaeed56be609ac341d
tichdaen <tichdaen@gmail.com>
2021-12-01 01:51:53 +0900

    ?

--- 짝지어 제거하기/JDH_pair_delete.js
+++ 짝지어 제거하기/JDH_pair_delete.js
@@ -10,5 +10,20 @@ rl.on('line', line => {
 }).on('close', () => process.exit());
 
 const solution = s => {
-    
+    if ( !/((\w)\2{1,})/g.test(s) ) {
+        console.log(-1); 
+        return 0;
+    }
+
+    let result = Array.from(s).reduce((p, c, i, a) => {
+        console.log(p, c);
+        if ( p === c ) {
+            return ''
+        }
+        return p + c;
+    });
+
+    console.log(`result : ${result} `);
+
+    return 1;
 }
